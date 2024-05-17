106c4b03535a7a01e5b0086abd33ca23d1459e7d
$HIVAM <supshiv7250@gmail.com>
2024-05-17 09:15:19 +0530

    Section 13 completed

--- ResortManagement.Web/Controllers/BookingController.cs
+++ ResortManagement.Web/Controllers/BookingController.cs
@@ -40,6 +40,52 @@ namespace ResortManagement.Web.Controllers
             return View(booking);
         }
 
+        //[Authorize]
+        //[HttpPost]
+        //public IActionResult FinalizeBooking(Booking booking)
+        //{
+        //    var villa = _unitOfWork.Villa.Get(u => u.Id == booking.VillaId);
+        //    booking.TotalCost = villa.Price * booking.Nights;
+        //    booking.Status = SD.StatusPending;
+        //    booking.BookingDate = DateTime.Now;
+
+        //    _unitOfWork.Booking.Add(booking);
+        //    _unitOfWork.Save();
+
+        //    var domain = Request.Scheme + "://" + Request.Host.Value + "/";
+        //    var options = new SessionCreateOptions
+        //    {
+        //        LineItems = new List<SessionLineItemOptions>(),
+        //        Mode = "payment",
+
+        //        SuccessUrl = domain + $"booking/BookingConfirmation?bookingId={booking.Id}",
+        //        CancelUrl = domain + $"booking/FinalizeBooking?villaId={booking.VillaId}&checkInDate={booking.CheckInDate}&nights={booking.Nights}"
+        //    };
+
+        //    options.LineItems.Add(new SessionLineItemOptions
+        //    {
+        //        PriceData = new SessionLineItemPriceDataOptions
+        //        {
+        //            UnitAmount = (long)(booking.TotalCost * 100),
+        //            Currency = "inr",
+        //            ProductData = new SessionLineItemPriceDataProductDataOptions
+        //            {
+        //                Name = villa.Name,
+        //               // Images = new List<string> { domain + villa.ImageUrl },
+        //            },
+        //        },
+        //        Quantity = 1,
+        //    });
+
+        //    var service = new SessionService();
+        //    Session session = service.Create(options);
+
+        //    _unitOfWork.Booking.UpdateStripePaymentID(booking.Id, session.Id, session.PaymentIntentId);
+        //    _unitOfWork.Save();
+        //    Response.Headers.Add("Location", session.Url);
+        //    return new StatusCodeResult(303);
+        //}
+
         [Authorize]
         [HttpPost]
         public IActionResult FinalizeBooking(Booking booking)
@@ -53,34 +99,78 @@ namespace ResortManagement.Web.Controllers
             _unitOfWork.Save();
 
             var domain = Request.Scheme + "://" + Request.Host.Value + "/";
-            var options = new SessionCreateOptions
-            {
-                LineItems = new List<SessionLineItemOptions>(),
-                Mode = "payment",
-                SuccessUrl = domain + $"/booking/BookingConfirmation?bookingId={booking.Id}",
-                CancelUrl = domain + $"/booking/FinalizeBooking?villaId={booking.VillaId}&checkInDate={booking.CheckInDate}&nights={booking.Nights}"
-            };
 
-            options.LineItems.Add(new SessionLineItemOptions
+            // Create a new customer in Stripe
+            var customerOptions = new CustomerCreateOptions
+            {
+                Name = booking.Name,
+                Address = new AddressOptions
+                {
+                    Line1 = "510 Townsend St",
+                    PostalCode = "98140",
+                    City = "San Francisco",
+                    State = "CA",
+                    Country = "US",
+                },
+            };
+            var customerService = new CustomerService();
+            var customer = customerService.Create(customerOptions);
+
+            // Create the payment session
+            var sessionOptions = new SessionCreateOptions
+            {
+                Customer = customer.Id, // Assign the created customer ID
+                LineItems = new List<SessionLineItemOptions>
+        {
+            new SessionLineItemOptions
             {
                 PriceData = new SessionLineItemPriceDataOptions
                 {
                     UnitAmount = (long)(booking.TotalCost * 100),
-                    Currency = "ind",
+                    Currency = "inr",
                     ProductData = new SessionLineItemPriceDataProductDataOptions
                     {
                         Name = villa.Name,
-                       // Images = new List<string> { domain + villa.ImageUrl },
+                        // Images = new List<string> { domain + villa.ImageUrl },
                     },
                 },
                 Quantity = 1,
-            });
-            return RedirectToAction(nameof(BookingConfirmation), new {bookingId = booking.Id});
+            }
+        },
+                Mode = "payment",
+                SuccessUrl = domain + $"booking/BookingConfirmation?bookingId={booking.Id}",
+                CancelUrl = domain + $"booking/FinalizeBooking?villaId={booking.VillaId}&checkInDate={booking.CheckInDate}&nights={booking.Nights}"
+            };
+
+            var sessionService = new SessionService();
+            Session session = sessionService.Create(sessionOptions);
+
+            _unitOfWork.Booking.UpdateStripePaymentID(booking.Id, session.Id, session.PaymentIntentId);
+            _unitOfWork.Save();
+            Response.Headers.Add("Location", session.Url);
+            return new StatusCodeResult(303);
         }
+
 
         [Authorize]
         public IActionResult BookingConfirmation(int bookingId)
         {
+            Booking bookingFromDb = _unitOfWork.Booking.Get(u => u.Id == bookingId, includeProperties: "User,Villa");
+
+            if(bookingFromDb.Status == SD.StatusPending)
+            {
+                //this is a pending order, we need to confirm if payment was successful
+
+                var service = new SessionService();
+                Session session = service.Get(bookingFromDb.StripeSessionId);
+
+                if(session.PaymentStatus == "paid")
+                {
+                    _unitOfWork.Booking.UpdateStatus(bookingFromDb.Id, SD.StatusApproved);
+                    _unitOfWork.Booking.UpdateStripePaymentID(bookingFromDb.Id,session.Id, session.PaymentIntentId);
+                    _unitOfWork.Save();
+                }
+            }
             return View(bookingId);
         }
     }
