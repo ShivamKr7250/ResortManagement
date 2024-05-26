d9f20fbb68d99666622c4fde50fdd0e248e57a97
$HIVAM <supshiv7250@gmail.com>
2024-05-26 12:37:53 +0530

    moduld 16 done

---
+++ ResortManagement.Web/ViewModels/PieChartVM.cs
@@ -0,0 +1,9 @@
+﻿
+namespace ResortManagement.Web.ViewModels
+{
+    public class PieChartVM
+    {
+        public decimal[] Series { get; set; }
+        public string[] Labels { get; set; }
+    }
+}
